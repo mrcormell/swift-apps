661a51ccf33c039fbec9104e1cf16a40df41168b
mrcormell <d.cormell@etoncollege.org.uk>
2019-12-02 11:15:14 +0000

    Initial check-in of empty console project

---
+++ TamagotchiConsole/TamagotchiConsole/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TamagotchiConsole
+//
+//  Created by David Cormell on 02/12/2019.
+//  Copyright © 2019 David Cormell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
